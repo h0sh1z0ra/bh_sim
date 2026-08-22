09855cf34c7dd40061023ba2f5f7f85449b640b0
h0sh1z0ra <kaelan.geng13@gmail.com>
2026-08-22 21:21:47 +0900

    Finished colour utils

--- bh_sim/color.h
+++ bh_sim/color.h
@@ -8,19 +8,17 @@
 using color = vec3;
 
 void write_color(std::ostream& out, const color& pixel_color) {
-                // Write to the real console; a pointer requires derefencing
-                // don't modify (const) nor copy (&) the color, just read
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
 
-    // Normalise to 255
-    int rbyte = int(255.999 * r);   // prevents overflow
+    // Translate the [0,1] component values to the byte range [0,255].
+    int rbyte = int(255.999 * r);
     int gbyte = int(255.999 * g);
     int bbyte = int(255.999 * b);
 
-    // Write out pixel color components
-    out << rbyte << ' ' << gbyte
+    // Write out the pixel color components.
+    out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
 }
 
 #endif
--- bh_sim/gradient.cpp
+++ bh_sim/gradient.cpp
@@ -1,5 +1,7 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>   // for std::cout (output)
-#include <vector>
 
 int main() {
     // Image
@@ -10,21 +12,33 @@ int main() {
     std::cout << "P3\n" << w << ' ' << h << "\n255\n";
 
     for (int y = 0; y < h; y++) {
-        // Progress bar
         std::clog << "\nScanlines remaining: " << (h - y) << ' ' << std::flush;
-
         for (int x = 0; x < w; x++) {
-            // Writing pixels out in rows; l -> r; t -> b
-            auto r = double(x) / (w-1);
-            auto g = double(y) / (h-1);
-            auto b = 0.25;
-
-            auto ir = int(255.999 * r);
-            auto ig = int(255.999 * g);
-            auto ib = int(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << "\n";
+            auto pixel_color = color(double(x)/(w-1),
+                                     double(y)/(h-1),
+                                     0);
+            write_color(std::cout, pixel_color);
         }
     }
     std::clog << "\nDone.               \n";
 }
+
+
+
+
+ // for (int y = 0; y < h; y++) {
+    //     // Progress bar
+    //     std::clog << "\nScanlines remaining: " << (h - y) << ' ' << std::flush;
+
+    //     for (int x = 0; x < w; x++) {
+    //         // Writing pixels out in rows; l -> r; t -> b
+    //         auto r = double(x) / (w-1);
+    //         auto g = double(y) / (h-1);
+    //         auto b = 0.25;
+
+    //         auto ir = int(255.999 * r);
+    //         auto ig = int(255.999 * g);
+    //         auto ib = int(255.999 * b);
+
+    //         std::cout << ir << ' ' << ig << ' ' << ib << "\n";
+    //     }
