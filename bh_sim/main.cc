8b78bbcc8944438e4fdf3cb38cd814d2342f3792
h0sh1z0ra <kaelan.geng13@gmail.com>
2026-09-06 19:24:04 +0900

    finished emscripten webassembly port

--- bh_sim/main.cc
+++ bh_sim/main.cc
@@ -72,7 +72,7 @@ int main() {
     // auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
     // world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
 
-    // auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    // auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0); Canada sorry maybe Japanese guys are shit then right like our couple's comment in Japan like
     // world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
 
     camera cam;
